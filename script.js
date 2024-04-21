a2d03f42d338f36e09a545f25e1ed9682f25c346
Arkaghoshg <144145423+Arkaghoshg@users.noreply.github.com>
2024-04-21 16:12:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    window.location.href = "response_yes.html";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    window.location.href = "response_no.html";
+});
